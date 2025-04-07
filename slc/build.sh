cd9d47b261f87ee7ff1681bbdd489cd1ccad824c
Michael Rupp <95718139+mykrupp@users.noreply.github.com>
2025-04-07 15:57:05 -0400

    remove post build

--- slc/build.sh
+++ slc/build.sh
@@ -36,7 +36,6 @@ SILABS_APP_PATH=$1
 SILABS_BOARD=$2
 CONFIG_ARGS=""
 BRD_ONLY=$(echo $SILABS_BOARD | cut -f1 -d";")
-export POST_BUILD_EXE=$(which commander)
 
 # Determine vars based on project type provided (.slcw solution example or .slcp project example file)
 if [[ "$SILABS_APP_PATH" == *.slcw ]]; then
